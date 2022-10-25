b599fbc36fa78897abc03183a634a08d3d0b6893
Vasanthreddy12345 <98875939+Vasanthreddy12345@users.noreply.github.com>
2022-10-25 20:05:22 +0530

    import java.util.*; class A{     public static void main(String args[]){         Scanner x=new Scanner(System.in);         int a=x.nextInt();         int b=x.nextInt();         int c=x.nextInt();         System.out.print(2*512*a*b*c);     } }

---
+++ Disk_Capacity.java
@@ -0,0 +1,10 @@
+import java.util.*;
+class A{
+    public static void main(String args[]){
+        Scanner x=new Scanner(System.in);
+        int a=x.nextInt();
+        int b=x.nextInt();
+        int c=x.nextInt();
+        System.out.print(2*512*a*b*c);
+    }
+}
