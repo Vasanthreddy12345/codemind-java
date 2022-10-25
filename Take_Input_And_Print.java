2660750576673ab3cb0846fae433f64da55df0b9
Vasanthreddy12345 <98875939+Vasanthreddy12345@users.noreply.github.com>
2022-10-25 19:59:05 +0530

    import java.util.*; class A{     public static void main(String args[]){         Scanner x=new Scanner(System.in);         String a=x.nextLine();         System.out.print(a);     } }

---
+++ Take_Input_And_Print.java
@@ -0,0 +1,8 @@
+import java.util.*;
+class A{
+    public static void main(String args[]){
+        Scanner x=new Scanner(System.in);
+        String a=x.nextLine();
+        System.out.print(a);
+    }
+}
