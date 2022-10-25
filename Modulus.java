fcb3be6d947ccac19d04de2c441b72db85b7d075
Vasanthreddy12345 <98875939+Vasanthreddy12345@users.noreply.github.com>
2022-10-25 19:54:22 +0530

    import java.util.*; class A{     public static void main(String args[]){         Scanner x=new Scanner(System.in);         int a=x.nextInt();         int b=x.nextInt();         System.out.print(a%b);     } }

---
+++ Modulus.java
@@ -0,0 +1,9 @@
+import java.util.*;
+class A{
+    public static void main(String args[]){
+        Scanner x=new Scanner(System.in);
+        int a=x.nextInt();
+        int b=x.nextInt();
+        System.out.print(a%b);
+    }
+}
